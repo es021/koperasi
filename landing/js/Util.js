a72d9de6065213b896cf463d6a722e9e0b1e8982
Wan Zulsarhan <zulsarhan.shaari@gmail.com>
2018-05-08 12:41:03 +0800

    A

--- landing/js/Util.js
+++ landing/js/Util.js
@@ -2,48 +2,48 @@ String.prototype.replaceAll = function (search, replace) {
     return this.replace(new RegExp(search, 'g'), replace);
 };
 
-class UtilClass {
-    constructor() {
-        this.PAGE_DEFAULT = "laman-utama";
-    }
-    _GET(sParam) {
-        var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-            sURLVariables = sPageURL.split('&'),
-            sParameterName,
-            i;
+var UtilClass = function () {
+    this.PAGE_DEFAULT = "laman-utama";
+};
 
-        var toRet = null;
+UtilClass.prototype._GET = function (sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
 
-        for (i = 0; i < sURLVariables.length; i++) {
-            sParameterName = sURLVariables[i].split('=');
+    var toRet = null;
 
-            if (sParameterName[0] === sParam) {
-                toRet = sParameterName[1] === undefined ? true : sParameterName[1];
-                break;
-            }
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            toRet = sParameterName[1] === undefined ? true : sParameterName[1];
+            break;
         }
-
-        if (sParam == "page" && !toRet) {
-            toRet = this.PAGE_DEFAULT;
-        }
-
-        return toRet;
     }
 
-    commentStyle(data) {
-        //comment out styling
-        data = data.replace("<style>", "<style>/*");
-        data = data.replace('<style type="text/css">', "<style>/*");
-        data = data.replace("<style type='text/css'>", "<style>/*");
-        data = data.replace('<style type="text/css" media="screen">', "<style>/*");
-        data = data.replace("</style>", "*/</style>");
-        return data;
+    if (sParam == "page" && !toRet) {
+        toRet = this.PAGE_DEFAULT;
     }
 
-    replaceRootPath(data) {
-        data = data.replaceAll("{{IMAGE_ROOT}}", IMAGE_ROOT);
-        data = data.replaceAll("{{SITE_ROOT}}", SITE_ROOT);
-        return data;
-    }
+    return toRet;
 }
+
+UtilClass.prototype.commentStyle = function (data) {
+    //comment out styling
+    data = data.replace("<style>", "<style>/*");
+    data = data.replace('<style type="text/css">', "<style>/*");
+    data = data.replace("<style type='text/css'>", "<style>/*");
+    data = data.replace('<style type="text/css" media="screen">', "<style>/*");
+    data = data.replace("</style>", "*/</style>");
+    return data;
+}
+
+UtilClass.prototype.replaceRootPath = function (data) {
+    data = data.replaceAll("{{IMAGE_ROOT}}", IMAGE_ROOT);
+    data = data.replaceAll("{{SITE_ROOT}}", SITE_ROOT);
+    return data;
+}
+
 var Util = new UtilClass();
